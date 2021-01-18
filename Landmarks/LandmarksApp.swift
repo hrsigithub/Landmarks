d60d13fe54cb25df31ef9da6384e3620eca4e317
ひろろ <hrsi.ad@gmail.com>
2021-01-18 10:58:57 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hiroshi.Nakai on 2021/01/18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
